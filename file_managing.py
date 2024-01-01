7fb94046e69bd0c95175e5f464eeb33b8469efdb
kerbeross93 <raul930604@gmail.com>
2024-01-01 17:35:46 +0000

    sdffsss

---
+++ file_managing.py
@@ -0,0 +1,9 @@
+import numpy as np
+a = np.array([0, 1, 2] )
+b = np.array([1, 2, 3])
+d = np.array(a * b)
+c = np.multiply(a,b)
+e = np.dot(a, b)
+print(d)
+print(c)
+print(e)
---
+++ readme.txt
@@ -0,0 +1,1 @@
+test my fck world
